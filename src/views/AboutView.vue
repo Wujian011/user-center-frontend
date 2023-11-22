3958d816c64d4c4265bcd2946964ef5c491e3318
wujian <2457507566@qq.com>
2023-11-22 22:38:22 +0800

    整合组建库

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button type="primary">Primary Button</a-button>
   </div>
 </template>
+<script setup lang="ts"></script>
