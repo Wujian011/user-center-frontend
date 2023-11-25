e1bcdb8d5ef539020836ff84d5b90eea783edbc1
zejun <zejun_sama@qq.com>
2023-11-26 01:06:21 +0800

    feat: 初始化

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import Antd from "ant-design-vue";
-import router from "./router";
-import "ant-design-vue/dist/reset.css";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = createApp(App);
-
-app.use(Antd).use(router).mount("#app");
+createApp(App).mount('#app')
--- src/views/AboutView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <a-button type="primary">Primary Button</a-button>
-  </div>
-</template>
-<script setup lang="ts"></script>
